e6ad2b64cbdb9dfb81f7e544bdcf548bd65222d3
Yasuyuki Komatsubara <kyasuyako@gmail.com>
2020-09-28 20:27:37 +0900

    Initial Commit

---
+++ QRCodeExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QRCodeExample
+//
+//  Created by Yasuyuki Komatsubara on 2020/09/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QRCodeExample/QRCodeExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeExampleApp.swift
+//  QRCodeExample
+//
+//  Created by Yasuyuki Komatsubara on 2020/09/28.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
